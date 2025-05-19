415e919d5785394e835a7a5b8c019afe7a83be87
kae <160863471+kktjss@users.noreply.github.com>
2025-05-19 12:10:16 +0300

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,8 +1,3 @@
-// Константы для путей к моделям
-export const MODEL_PATHS = {
-    MIKU: `${process.env.PUBLIC_URL}/models/hatsune_miku.glb`,
-};
-
 // URL API бэкенда
 export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
